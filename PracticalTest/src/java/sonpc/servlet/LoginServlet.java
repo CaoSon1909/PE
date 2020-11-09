d92e483b2852a2b68fac324ff9da68875bb95f0f
UGjhuqFtIENhbyBTxqFu <@SE140012>
2020-11-09 09:22:15 +0700

    Commit at 637405105351812002 - 1194.7359427s

--- PracticalTest/src/java/sonpc/servlet/LoginServlet.java
+++ PracticalTest/src/java/sonpc/servlet/LoginServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import sonpc.tblUser.TblUserDAO;
 
 /**
  *
@@ -37,7 +38,9 @@ public class LoginServlet extends HttpServlet {
         try{
             String username = request.getParameter("txtUsername");
             String password = request.getParameter("txtPassword");
-            
+            //dao
+            TblUserDAO dao = new TblUserDAO();
+            boolean result = dao.checkLogin(username, password)
         }
         finally{
             request.getRequestDispatcher(url).forward(request, response);
